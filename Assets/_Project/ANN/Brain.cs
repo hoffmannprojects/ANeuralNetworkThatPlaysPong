8964a43a7061b008f31087522e5bd5eddac6c025
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-06-11 23:22:57 +0200

    Added Run() to Brain.cs

--- Assets/_Project/ANN/Brain.cs
+++ Assets/_Project/ANN/Brain.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Brain : MonoBehaviour 
+public class Brain : MonoBehaviour
 {
     [SerializeField]
     private GameObject _paddle;
@@ -23,15 +23,45 @@ public class Brain : MonoBehaviour
 
 
     // Use this for initialization
-    private void Start () 
-	{
+    private void Start()
+    {
         _ann = new ANN(6, 1, 1, 4, 0.11);
         _ballRigidbody2D = _ball.GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	private void Update () 
-	{
-		
-	}
+    }
+
+    private List<double> Run(
+        double ballXPosition,
+        double ballYPosition,
+        double ballXVelocity,
+        double ballYVelocity,
+        double paddleXPosition,
+        double paddleYPosition,
+        double paddleVelocity,
+        bool train)
+    {
+        var inputs = new List<double>();
+        var outputs = new List<double>();
+        inputs.Add(ballXPosition);
+        inputs.Add(ballYPosition);
+        inputs.Add(ballXVelocity);
+        inputs.Add(ballYVelocity);
+        inputs.Add(paddleXPosition);
+        inputs.Add(paddleYPosition);
+        outputs.Add(paddleVelocity);
+
+        if (train)
+        {
+            return _ann.Train(inputs, outputs);
+        }
+        else
+        {
+            return _ann.CalcOutput(inputs, outputs);
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
 }
